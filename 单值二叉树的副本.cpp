a8b93e5d572b3c43ed27e85174cb81602c00609d
lvningyuan <571533258@qq.com>
2020-01-04 22:01:44 +0800

    。

---
+++ 单值二叉树的副本.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if(root==NULL)
+        return true;
+      if(root->left  && root->left ->val !=root->val)／／注意 判不等才能停止
+      return false;
+      if(root->right && root->right->val != root-> val)／／判不等才能停止
+      return false;
+      return isUnivalTree(root->left)&&isUnivalTree(root->right);
+
+    }
+};
